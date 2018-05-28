b4fb21a76902ffe37e315ffa20a0f3ed7074351d
Alex <alexheistdev@gmail.com>
2018-05-28 12:02:58 -0600

    partial

--- mydj/models.py
+++ mydj/models.py
@@ -3,14 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-<<<<<<< Updated upstream
 class Profile(models.Model):
-<<<<<<< Updated upstream
-=======
-'''class Profile(models.Model):
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	is_exhibitor= models.BooleanField(default=False)
 	company_name = models.TextField(max_length=200, blank=True)
@@ -23,7 +16,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-	instance.profile.save()'''
+	instance.profile.save()
 
 class Booth(models.Model):
 	booth_name= models.BooleanField(default=False)
