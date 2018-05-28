c86f8f428371a98f843ccd4754a591516f19833c
Alex <alexheistdev@gmail.com>
2018-05-28 11:44:14 -0600

    Merging in Alex work

--- mydj/forms.py
+++ mydj/forms.py
@@ -2,15 +2,20 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile, Store
 
+class SignUpForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields = ('username', 'password', 'first_name', 'last_name', 'email')
+	
 class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('username', 'password', 'first_name', 'last_name', 'email')
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email')
 
 class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('is_exhibitor', 'company_name')
+	class Meta:
+		model = Profile
+		fields = ('is_exhibitor', 'company_name')
 
 class StoreEditForm(forms.ModelForm):
 	class Meta:
--- mydj/views.py
+++ mydj/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .forms import UserForm, ProfileForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .forms import ProfileForm, UserForm
+from .models import Profile
 
 def signup(request):
 	if request.method == 'POST':
-		user_form = UserForm(request.POST)
-		if user_form.is_valid():
-			user_form.save()
-		return HttpResponseRedirect('profile.html')
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			login(request, user)
+			return HttpResponseRedirect('profile.html')
 	else:
-		user_form = UserForm()
-		return render(request, 'pages/signup.html', { 'form': UserForm })
+		form = UserCreationForm()
+		return render(request, 'pages/signup.html', { 'form': form })
 
 def expo(request):
 	expo=""
@@ -32,19 +40,20 @@ def edit(request):
 #@login_required
 #@transaction.atomic
 def update_profile(request):
+    profile_info = Profile.objects.get(user=request.user)
     if request.method == 'POST':
         user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=profile_info)
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, _('Your profile was successfully updated!'))
-            return redirect('settings:profile')
+            messages.success(request, 'Your profile was successfully updated!')
+            return HttpResponseRedirect('profile.html')
         else:
-            messages.error(request, _('Please correct the error below.'))
+            messages.error(request, 'Please correct the error below.')
     else:
         user_form = UserForm(instance=request.user)
-        profile_form = ProfileForm(instance=request.user)
+        profile_form = ProfileForm(instance=profile_info)
     return render(request, 'pages/profile.html', {
         'user_form': user_form,
         'profile_form': profile_form
